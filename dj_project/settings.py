40798cc54e3716b8322d90d29446a90fc9e7eb5b
Rhizome23 <rhizome23@free.fr>
2019-12-24 23:37:44 +0100

    modifié :         dj_project/settings.py

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -36,11 +36,11 @@ else:
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL ='/static/'
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(BASE_DIR, 'static'),
     )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
