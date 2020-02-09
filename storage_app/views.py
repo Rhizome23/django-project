4c993b42fe294cc050b08a868e02a69dd2aef829
Rhizome23 <rhizome23@free.fr>
2020-02-09 23:50:59 +0100

    modifié :         Procfile 	modifié :         dj_project/settings.py 	modifié :         dj_project/urls.py 	modifié :         requirements.txt 	modifié :         storage_app/views.py

--- storage_app/views.py
+++ storage_app/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import PostForm, PictureForm
 
-
 # Import the exporter
 import markdown
 import nbformat
