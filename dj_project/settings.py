85800b0823d7919ef4cc8611004ba05305365346
Rhizome23 <rhizome23@free.fr>
2020-02-11 21:10:50 +0100

    modifié :         dj_project/settings.py

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -95,8 +95,8 @@ else :
     pass
 
 # Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
